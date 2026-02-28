06d77b78901d83398be97049d999e95893d3f821
Brendan Tierney <tiernebre@gmail.com>
2026-02-28 17:03:16 -0500

    fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+alias vim "nvim"
+
+function config --wraps git --description "Manages dotfiles"
+    git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv
+end
+
+
