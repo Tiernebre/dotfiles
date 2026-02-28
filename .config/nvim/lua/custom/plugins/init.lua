6c7a3b2d43b0a32e27d0c0e95018160b1a2121e7
Brendan Tierney <tiernebre@gmail.com>
2026-02-28 17:01:49 -0500

    initial commit + nvim config

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = { 'markdown' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+}
